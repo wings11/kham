e330ac6ef1f98362951c5ebec7c3877a754b0392
wings11 <wykyaw2001@gmail.com>
2025-03-15 16:10:36 +0700

    gg

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import cvFile from '../assets/cv.pdf';
 
 const AboutMe = () => {
--- src/components/AcademicRecord.js
+++ src/components/AcademicRecord.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 
 const AcademicRecord = () => {
   const academicRef = useRef();
